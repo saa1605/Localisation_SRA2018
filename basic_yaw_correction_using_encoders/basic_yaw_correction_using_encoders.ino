940a2aecbf4ecd07d4a6527d5ce7e275f63e469b
Saaket Agashe <saaketAgashe@Saakets-MacBook-Air.local>
2018-04-08 14:46:31 +0530

    added linear correction

--- basic_yaw_correction_using_encoders/basic_yaw_correction_using_encoders.ino
+++ basic_yaw_correction_using_encoders/basic_yaw_correction_using_encoders.ino
@@ -6,39 +6,36 @@
 
 #define MOTORR 
 
-float pi = 3.1415;
 
+//Fixed Parameter
+float pi = 3.1415;
 volatile long int ticksL = 0;
 volatile long int ticksR = 0;
-
 float radius_wheel = 3.3;
 float radius_bot = 21.3/2;
-
-float number_of_rotations = 0;
-float bot_angle = 0;
-float dest_angle = 0;
 int ppr = 60;
-
-int dest_x = 10;
-int dest_y = 10;
-int init_x = 0;
-int init_y = 0;
-
-float yaw_error = 0;
-
-int yaw_value,yaw_pwm = 0;
 int yaw_scaling_factor = 0.5;
 int linear_displacement_scaling_factor = 0.5;
 
-float averageEncoderReadings(int ticksLeft, int ticksRight)
-{
- return (ticksLeft + ticksRight) / 2; 
-}
+//Dynamic Parameters
+float number_of_rotations = 0;
+float destination_theta = 0,current_theta = 0;
+int destination_r = 0,current_r=0;
 
-void yaw_correction(float destination_angle, float initial_angle)
+
+float yaw_error = 0;
+int linear_error = 0;
+
+
+
+//float averageEncoderReadings(int ticksLeft, int ticksRight)
+//{
+// return (ticksLeft + ticksRight) / 2; 
+//}
+
+void yaw_correction()
 {
   
-  yaw_error = degrees(destination_angle - initial_angle);
   if(yaw_error>0)
   {
     bot_spot_left();
@@ -47,50 +44,45 @@ void yaw_correction(float destination_angle, float initial_angle)
   {
     bot_spot_right();
   }
-  yaw_error = abs(yaw_error);
-  yaw_value = yaw_error;
+  
+  yaw_value = abs(yaw_error);
   yaw_pwm = map(yaw_value,0,pi,100,400);
-
-  set_pwm1a(yaw_pwm);
-  set_pwm1b(yaw_pwm);
    
+}
+
+void linear_correction()
+{
+  linear_value = linear_error;
+  linear_pwm = map(linear_value,0,1000,100,400);
 }
 
 int main()
 {
   Serial.begin(115200);
-  MCUCR |= (1 << ISC01) ;
+  MCUCR |= (1 << ISC01);
   GICR |= (1 << INT0) | (1 << INT1);
   DDRD = 0x00;
   PORTD = (1<<2)|(1<<3)|(1<<4)|(1<<5);
   pwm1_init();
   sei();
+  destination_theta = atan2(destination_y-initial_y,destination_x-initial_x);
+  destination_r = sqrt((destination_y-current_y)*(destination_y-current_y) + (destination_x-current_x)*(destination_x-current_x));
 
   while (1)
-  {
-
-    dest_angle = atan2(dest_y-init_y,dest_x-init_x);
-
-    
-    while(bot_angle != dest_angle)
+  { 
+    number_of_rotations = averageEncoderReadings(ticksL,ticksR)/ppr;
+    current_theta = (2*pi*radius_wheel/radius_bot)*number_of_rotations;
+    yaw_error = destination_theta - current_theta;
+    linear_error = number_of_rotations*2*pi;
+    if(-2<yaw_error<2)
     {
-      if(dest_angle-bot_angle>0)
-      {
-        bot_spot_left();
-      }
-      else
-      {
-        bot_spot_right();
-      }
-
-       number_of_rotations = averageEncoderReadings(ticksL,ticksR)/ppr;
-       bot_angle = (2*pi*radius_wheel/radius_bot)*number_of_rotations; 
+      yaw_correction(destination_theta,current_theta); 
     }
-
-    init_x = dest_x;
-    init_y = dest_y;
+    if(-2<linear_error<2)
+    {
+      linear_correction();
+    }
   }
-
 }
 
 ISR(INT0_vect)
@@ -116,4 +108,8 @@ ISR(INT1_vect)
     ticksR--;
   }
 }
+
+/*       number_of_rotations = averageEncoderReadings(ticksL,ticksR)/ppr;
+       bot_angle = (2*pi*radius_wheel/radius_bot)*number_of_rotations;
+*/
 
